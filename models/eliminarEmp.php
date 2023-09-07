55c78597f6b1e83783d68e5ceecae2b089980526
NicoVivas27 <sr.nicolas1928@gmail.com>
2023-09-07 00:00:26 -0500

    Arreglo de error

--- models/eliminarEmp.php
+++ models/eliminarEmp.php
@@ -3,14 +3,30 @@
     $con = connect();
     $id = $_GET['id'];
 
-        $consulta ="DELETE FROM empleado 
-                    WHERE id = '$id'";
+        $consulta ="DELETE FROM nomina WHERE id_empleado = '$id'";
         $result = mysqli_query($con, $consulta);
         if($result){
-          echo '<script>alert("Se elimino correctamente el empleado")</script>';
-          header('Location: ./../index.php');
+            $consulta ="DELETE FROM devengado WHERE id = '$id'";
+            $result1 = mysqli_query($con, $consulta);
+            if($result1){
+                $consulta ="DELETE FROM deducciones WHERE id = '$id'";
+                $result2 = mysqli_query($con, $consulta);
+                if($result2) {
+                    $consulta ="DELETE FROM prestaciones_sociales WHERE id = '$id'";
+                    $result3 = mysqli_query($con, $consulta);
+                if($result3) {
+                    $consulta ="DELETE FROM empleado WHERE id = '$id'";
+                    $result4 = mysqli_query($con, $consulta);
+                    if($result4) {
+                        echo '<script>alert("Se elimino correctamente el empleado")</script>';
+                        header('Location: ./../index.php');
+                        }
+                    }
+                }
+            }
         }else{
             echo '<script>alert("ERROR: No se elimino el empleado")</script>';
+            header('Location: ./../index.php');
         }
 
     ?>
